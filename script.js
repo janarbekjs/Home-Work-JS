11e224fb925b62d703ef438288450f2a1a64c741
janarbek <janarbekoljobaev55@gmail.com>
2024-07-17 03:29:10 +0300

    finished домашная работа

---
+++ script.js
@@ -0,0 +1,46 @@
+const titleName = "Участники и их баллы:";
+console.log(titleName);
+let firstUserName = "1. Nooruz-97";
+let firstUserPoin = "баллов из 100";
+let combinet = `${firstUserName} ${firstUserPoin}`;
+console.log(combinet);
+let secondUserName = "2. Zhakshylyk-89";
+let secondUserPoint = " баллов из 100";
+let result = secondUserName + "" + secondUserPoint;
+console.log(result);
+let thirdUserName = "3. Alisher-75";
+let thirdUserPoint = "баллов из 100";
+let greatJob = `${thirdUserName} ${thirdUserPoint}`;
+console.log(greatJob);
+let fourthUserName = "4. Elizar-90";
+let fourthUserPoint = " баллов из 100";
+let total = fourthUserName + "" + fourthUserPoint;
+console.log(total);
+const itsAlover = "Средние баллы в экзамне:";
+const messeng = "87.75 Балл:";
+const totalAnd = `${itsAlover} ${messeng}`;
+console.log(totalAnd);
+// Table
+const number = 6;
+console.log("1x" + number + "=" + number * 1);
+console.log("2x" + number + "=" + number * 2);
+console.log("3x" + number + "=" + number * 3);
+console.log("4x" + number + "=" + number * 4);
+console.log("5x" + number + "=" + number * 5);
+console.log("6x" + number + "=" + number * 6);
+console.log("7x" + number + "=" + number * 7);
+console.log("8x" + number + "=" + number * 8);
+console.log("9x" + number + "=" + number * 9);
+console.log("10x" + number + "=" + number * 10);
+// Brauzers
+let info = "Вы успешно зарегистрировались!";
+alert(info);
+
+let information = confirm(
+  "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+);
+alert(information);
+
+let myName = prompt("Введите ваше имя");
+const totalName = "Janarbek";
+console.log(myName);
