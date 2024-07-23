6c8156f88109e22444671af2796c14a888d108cf
janarbek <janarbekoljobaev55@gmail.com>
2024-07-23 17:21:03 +0300

    hop

---
+++ script.js
@@ -0,0 +1,26 @@
+// Варянт 1
+// function numbers(x, y) {
+//     for (let index = x; index <= y; index++) {
+//       console.log(index);
+//     }
+//   }
+//   numbers(1,500);
+
+// Варянт 2
+
+// const  logToConsole = (a, b) => {
+//   for (let index = a; index <= b; index++) {
+//     console.log(index);
+//   }
+// };
+//  logToConsole(7, 79);
+
+// Варянт 3
+
+// let logToConsole = function (j, k) {
+//   for (let index = j; index <= k; index++) {
+//     console.log(index);
+//   }
+// };
+// logToConsole(1, 90);
+
