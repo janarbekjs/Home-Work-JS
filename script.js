1edb2980957b8e5aa05a8e5c4f8aa44421c4b43f
janarbek <janarbekoljobaev55@gmail.com>
2024-07-25 22:46:52 +0300

    Finished home-work-5

--- script.js
+++ script.js
@@ -1,144 +1,56 @@
-//! задача-1, Declaration function
+//! Zadacha 1
 
-// function numbers(x, y) {
-//     for (let index = x; index <= y; index++) {
-//       console.log(index);
-//     }
-//   }
-//   numbers(1,30);
-
-//! задача-1, arrow function
-
-// const  logToConsole = (a, b) => {
-//   for (let index = a; index <= b; index++) {
-//     console.log(index);
-//   }
+// const createStudent = {
+//   firstName: "Janarbek",
+//   lastName: "Oljobaev",
+//   age: 34,
+//   edication: {
+//     university: "ALA TOO",
+//     faculty: "JS",
+//     gpa: 63,
+//   },
 // };
-//  logToConsole(7, 79);
+// console.log(createStudent);
 
-//! задача-1, expression function
+//! Zadacha 2
 
-// const logToConsole = function (j, k) {
-//   for (let index = j; index <= k; index++) {
-//     console.log(index);
-//   }
-// };
-// logToConsole(1,90);
-
-//! задача-2, Declaration function
-
-// function showEvenNumbers2(number, number2) {
-//   for (let n = number; n < number2; n++)
-//     if (n % 2 === 0)
-//         console.log(n);
-// }
-
-// showEvenNumbers2(1,100);
-
-//! задача-2, arrow function
-
-// const arrow2 = (num1, num2) => {
-//   for (let h = num1; h < num2; h++)
-//     if (h % 2 === 0)
-//     console.log(h);
-// };
-// arrow2(1, 100);
-
-//! задача-2, expression function
-
-// const expressN = function(numbers1, numbers2){
-//    for(let s = numbers1; s < numbers2; s++)
-//           if (s %2 === 0 )
-//           console.log(s);
-// }
-// expressN(5,77)
-
-//! задача-3, Declaration function
-
-// function sum(san, san2) {
-//   let res = 0;
-
-//   for (let a = san; a < san2; a++) {
-//     if (a % 2 === 0) {
-//       res = res + a;
-//     }
-//   }
-//   return res;
-// }
-// const result1 = sum(1,100);
-// console.log(result1);
-
-//! задача-3, expression function
-
-//     const expressionS = function (san, san2) {
-//       let res = 0;
-//       for (let a = san; a < san2; a++) {
-//         if (a % 2 === 0) {
-//           res = res + a;
-//         }
-//       }
-//       return res;
-//     };
-//     const result2 = expressionS(23, 80);
-//     console.log(result2);
-
-//! задача-3, arrow function
-
-//     const arrowF = (san1, san2) => {
-//       let res = 0;
-//       for (let a = san1; a < san2; a++) {
-//         if (a % 2 === 0) {
-//           res = res + a;
-//         }
-//       }
-//       return res;
-//     };
-//     const result3 = arrowF(5,43);
-//     console.log(result3);
-
-//!задача-4, Declaration function
-
-// function userN() {
-//   let userName = "Bill Gates";
-//   for (let i = 0; i < userName.length; i++) {
-//     const char = userName.charAt(i);
-//     if (char === "e") {
-//       console.log(`${char}`);
-//     }
+// function deleteKey(object, key) {
+//   if (key in object) {
+//     delete object[key];
+//     return object;
+//   } else {
+//     return `Kluch${key} otsutstvuet v obekte`;
 //   }
 // }
-// userN();
+// let object2 = { name: "Janarbek", faculty: "JS", gpa: 63 };
+// console.log(deleteKey(object2, "name"));
+// console.log(deleteKey(object2, "gpa"));
 
-//!задача-5, Declaration function
+//! Zadacha 3
 
-// function users(text) {
-//   let reversedStrFor = "";
-//   let string = "Hello";
-//   for (let index = string.length - 1; index >= 0; index--) {
-//     reversedStrFor += string[index];
-//   }
-//   console.log(reversedStrFor);
+// const user = {
+//   name: "Janarbek",
+//   email: "janarbek@gmail.com",
+//   age: 30,
+//   password: 123456,
+// };
+
+// const num = 5;
+// const userEmail = prompt("Enter your email!");
+// if (userEmail === user.email) {
+//   alert("Wellcome");
+// } else {
+//   alert("Ty paputal");
 // }
-// users();
-
-//!задача-5, expression function
-
-// const expressionFun = function (word) {
-//   let koin = word.length;
-//   while (koin >= 0) {
-//     console.log(word.charAt(coun));
-//     koin--;
-//   }
-// };
-// expressionFun("Жанарбек");
-
-//!задача-5, arrow function
-
-// const arrowFun5 = (dodj) => {
-//   let contr = dodj.length;
-//   while (contr >= 0) {
-//     console.log(dodj.charAt(coun));
-//     contr--;
-//   }
-// };
-// arrowFun5("Нормально элеби");
+// const userPassword = prompt("Enter your password");
+// if (userPassword === user.password) {
+//   alert("Wellcome");
+// } else {
+//   alert("Ty paputal");
+// }
+// const addUser = +prompt("3+2=?");
+// if (addUser === num) {
+//   (user.num = 5), alert("Verno bratka");
+// } else {
+//   alert("Ne verno bratka");
+// }
