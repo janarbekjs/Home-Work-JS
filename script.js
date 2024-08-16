f4adec2098be2046b0e21df5bdf94418bfb0d2aa
janarbek <janarbekoljobaev55@gmail.com>
2024-08-16 22:41:49 +0300

    пока буто елек

---
+++ script.js
@@ -0,0 +1,18 @@
+//! Task 1
+// const zagolovok = document.getElementById("zagolovok");
+
+//! Task 2
+// const conteiner = document.getElementById("conteiner");
+
+//! Task 3
+const minutes = document.getElementById("minutes");
+
+const seconds = document.getElementById("seconds");
+
+const timeStart = document.getElementById("time-start");
+
+const timeRestart = document.getElementById("time-restart");
+
+const timePause = document.createElement("time-pause");
+
+const time = document.getElementById("time");
