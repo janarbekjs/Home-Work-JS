ed1b284492ba7b0d66dd440142aa1f1e9d4e491c
janarbek <janarbekoljobaev55@gmail.com>
2024-08-17 17:37:45 +0300

    Finished Home-work-13

--- script.js
+++ script.js
@@ -1,18 +1,75 @@
 //! Task 1
 // const zagolovok = document.getElementById("zagolovok");
+// setInterval(() => {
+//   zagolovok.textContent = "Ураа";
+//   console.log(setInterval);
+// }, 5000);
 
 //! Task 2
 // const conteiner = document.getElementById("conteiner");
+// let count = ["red", "blue", "black", "green", "aqua", "chocolate"];
+// let result = 0;
+// function inter() {
+//   conteiner.style.backgroundColor = count[result];
+//   result++;
+//   if (result > backgroundColor.length - 1) {
+//     result = 0;
+//   }
+// }
+// setInterval(inter, 1000);
 
 //! Task 3
-const minutes = document.getElementById("minutes");
+// const minutes = document.getElementById("minutes");
+// const seconds = document.getElementById("seconds");
+// const timeStart = document.getElementById("time-start");
+// const timeRestart = document.getElementById("time-restart");
+// const time = document.getElementById("time");
 
-const seconds = document.getElementById("seconds");
+// let interval;
+// let showButton;
+// let secondsValue;
+// let minutesValue;
 
-const timeStart = document.getElementById("time-start");
+// minutes.addEventListener("input", (event) => {
+//   minutesValue = Number(event.target.value);
+// });
 
-const timeRestart = document.getElementById("time-restart");
+// seconds.addEventListener("input", (event) => {
+//   secondsValue = Number(seconds.value);
+// });
 
-const timePause = document.createElement("time-pause");
-
-const time = document.getElementById("time");
+// timeStart.addEventListener("click", () => {
+//   if (minutes.value.trim().length === 0 && seconds.value.trim().length === 0) {
+//     return alert("Попробуй написать цыфру...");
+//   }
+//   if (!showButton) {
+//     timeStart.textContent = "PAUSE";
+//     showButton = true;
+//     interval = setInterval(() => {
+//       if (secondsValue > 0) {
+//         secondsValue--;
+//       } else {
+//         secondsValue = 59;
+//         minutesValue--;
+//       }
+//       if (minutesValue < 0) {
+//         clearInterval(interval);
+//       }
+//       time.textContent = `${
+//         minutesValue < 10 ? "0" + minutesValue : minutesValue
+//       }:${secondsValue < 10 ? "0" + secondsValue : secondsValue}`;
+//     }, 1000);
+//   } else {
+//     timeStart.textContent = "START";
+//     clearInterval(interval);
+//     showButton = false;
+//   }
+// });
+// timeRestart.addEventListener("click", () => {
+//   clearInterval(interval);
+//   time.textContent = "00:00";
+//   minutesValue = 0;
+//   secondsValue = 0;
+//   timeStart.textContent = "START";
+//   showButton = false;
+// });
