98caa404077bb379d8846e51185810376d8c942e
janarbek <janarbekoljobaev55@gmail.com>
2024-08-21 16:47:59 +0300

    ok

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-const div = document.getElementById("div");
+const ul = document.getElementById("ul-list");
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 
 const BASE_URL = "https://50bcaa30f3d758cf.mokky.dev";
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  const todoValue = input.value.trim();
-  if (todoValue) {
-    const newTodo = {
-      id: Date.now().toString(),
-      title: todoValue,
-    };
-    input.value = "";
-    postTodo(newTodo);
-  }
-});
 
 const postTodo = async (objectTodo) => {
   try {
@@ -38,12 +25,7 @@ const postTodo = async (objectTodo) => {
 
 const getTodos = async (newArray) => {
   try {
-    const response = await fetch(`${BASE_URL}/items`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(`${BASE_URL}/items`, {});
     const result = await response.json();
     if (response.status === 200) {
     }
@@ -53,19 +35,53 @@ const getTodos = async (newArray) => {
   }
 };
 
-const renderTodos = (newTodo = []) => {
-  div.innerHTML = "";
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const todoValue = input.value.trim();
+  if (todoValue) {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: todoValue,
+    };
+    input.value = "";
+    postTodo(newTodo);
+  }
+});
+
+const renderTodos = (newTodo) => {
+  ul.innerHTML = "";
 
   newTodo.map((item) => {
-    const p = document.createElement("p");
-    p.textContent = item.text;
-    const button1 = document.createElement("button");
-    button1.textContent = "DELETE";
-    p.textContent = item.title;
+    const li = document.createElement("li");
+    li.textContent = item.title;
 
-    button1.addEventListener("click", () => buttonDelete(item.id));
-    p.appendChild(button1);
-    div.appendChild(p);
+    const input2 = document.createElement("input");
+    input2.type = "checkbox";
+
+    const buttonUp = document.createElement("button");
+    buttonUp.className = "buttonUp";
+    buttonUp.textContent = "UPDATE";
+
+    const buttonDe = document.createElement("button");
+    buttonDe.className = "buttonDe";
+    buttonDe.textContent = "DELETE";
+
+    input2.addEventListener("click", () => {
+      if (input2.checked) {
+        li.style.textDecoration = "line-through";
+        li.style.textDecorationColor = "red";
+        localStorage.setItem("todos", JSON.stringify(newArray));
+      } else {
+        li.style.textDecoration = "none";
+      }
+    });
+    buttonDe.addEventListener("click", () => buttonDelete(item.id));
+    buttonUp.addEventListener("click", () => {
+      updateTodo(item.id.newArray);
+    });
+
+    li.append(input2, buttonUp, buttonDe);
+    ul.appendChild(li);
   });
 };
 
@@ -75,11 +91,33 @@ const buttonDelete = async (id) => {
       method: "DELETE",
     });
     if (response.status === 200) {
-      // console.log("Request is succesfull");
+      console.log("Request is succesfull");
     }
     getTodos();
   } catch {
-    error;
+    console.log(error);
+  }
+};
+
+const updateTodo = async (id, newTodo) => {
+  const newFind = newTodo.find((item) => item, id);
+  input.value = newFind.value;
+  input.focus();
+  try {
+    const response = await fetch(`${BASE_URL}/items/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application",
+      },
+      body: JSON.stringify({
+        title: input.value,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    getTodos(id);
+  } catch (error) {
+    console.log(error);
   }
 };
 getTodos();
